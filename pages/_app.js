e2461f3a7bfeb3d1a0ed263f340b80b774c24996
Andrey Reznikov <andreyrez02@gmail.com>
2022-05-11 13:18:33 +0300

    add dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,34 @@ import "swiper/css";
 import "swiper/css/pagination";
 import Layout from './components/layout'
 import { SessionProvider } from 'next-auth/react'
+import { useMemo } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
 function MyApp({ Component,
   pageProps: { session, ...pageProps },
 }) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </SessionProvider>
   )
 }
